b8bac5f34bbd2e152624ff93348c3f7f4b66b731
Jesus Glez <jesus.gonzalez@merida.gob.mx>
2025-01-24 10:37:26 -0600

    feat(App): :wrench: Preconfiguracion de app

---
+++ resources/js/modules/App.jsx
@@ -0,0 +1,10 @@
+const App = () => {
+    return (
+        <div>
+            <h1>Welcome</h1>
+            <p>This is a basic React component.</p>
+        </div>
+    );
+};
+
+export default App;
--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,48 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import fs from "fs";
+import { defineConfig, loadEnv } from "vite";
+import laravel from "laravel-vite-plugin";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
-export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-        }),
-    ],
+export default defineConfig(({ command, mode }) => {
+    const env = loadEnv(mode, process.cwd(), "");
+    const SERVE_HOST_ENABLED = !!env.SERVE_HOST;
+
+    let httpsConfig = false;
+
+    if (SERVE_HOST_ENABLED) {
+        try {
+            httpsConfig = {
+                key: fs.readFileSync(env.SERVE_HTTPS_KEY),
+                cert: fs.readFileSync(env.SERVE_HTTPS_CERT),
+            };
+        } catch (error) {
+            console.error(
+                "Error reading HTTPS key or certificate files:",
+                error
+            );
+        }
+    }
+
+    return {
+        server: SERVE_HOST_ENABLED
+            ? {
+                  host: env.SERVE_HOST,
+                  port: 5173,
+                  https: httpsConfig,
+              }
+            : false,
+        plugins: [
+            laravel({
+                input: [
+                    "resources/css/app.css",
+                    "resources/js/app.js",
+                    "resources/js/app/index.jsx",
+                ],
+                refresh: true,
+            }),
+            react(),
+            tailwindcss(),
+        ],
+    };
 });
