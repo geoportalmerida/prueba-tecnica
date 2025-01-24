6d60231369a565bd0354413bffef526046343ae8
Jesus Glez <jesus.gonzalez@merida.gob.mx>
2025-01-24 10:59:48 -0600

    feat(App): 🎉 Mejora de estilos y mensajes

--- resources/js/modules/App.jsx
+++ resources/js/modules/App.jsx
@@ -1,8 +1,9 @@
 const App = () => {
     return (
-        <div>
-            <h1>Welcome</h1>
-            <p>This is a basic React component.</p>
+        <div className="text-center h-svh w-full flex flex-col justify-center items-center">
+            <h1 className="text-lg font-bold">Welcome 👋</h1>
+            <p className="mb-4">This is a basic React component 🚀</p>
+            <p>Happy Coding 🎉</p>
         </div>
     );
 };
